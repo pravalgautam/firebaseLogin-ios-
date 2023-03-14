eae7074e1292e8fbaa5e8640eb57c92633e85a70
pravalgautam <pravalgautam79@gmail.com>
2023-03-14 15:11:37 +0530

    Initial Commit

---
+++ loginpage/loginpageApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginpageApp.swift
+//  loginpage
+//
+//  Created by Praval Gautam on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct loginpageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
